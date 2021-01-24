d4d53ef416c6af379f985f0abf48a2ab5ec792d0
Abhay goel <77920915+abhaygoel8@users.noreply.github.com>
2021-01-24 20:09:09 +0530

    created switch.c
    this file show c switch statement is fall through

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int number=0;
+printf("enter a mumber:");
+scanf("%d",&number);
+switch(number){
+case 10:
+print("number is equal to 10");
+break;
+case 50:
+printf("number is equal to 100");
+break;
+default:
+print("number is not equal to 10, 50 or 100);
+}
+return 0;
+}
